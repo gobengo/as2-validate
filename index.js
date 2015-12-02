49748d5aaf355f07bf1abe5a59025612ad28e023
Benjamin Goering <bengoering@gmail.com>
2015-12-02 14:05:40 -0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var jsonstream = require('jsonstream')
+var through = require('through3')
+var validator = require('activitystreams.validator')
+var map = require('stream-transform').map
+
+var ActivityToValidationResult = through.transform(function (a, enc, done) {
+  try {
+    done(null, validator.validate(a))
+  } catch (e) {
+    done(e)
+  }  
+}, null, { objectMode: true })
+
+var Stringifier = through.transform(function transform(chunk, enc, done) {
+  done(null, chunk.toString())  
+})
+
+function exitOnError(stream) {
+  stream.on('error', function (err) {
+    console.error(err);
+    process.exit()
+  })
+  return stream;
+}
+
+process.stdin
+.pipe(exitOnError(jsonstream.parse()))
+.pipe(exitOnError(map(validator.validate)))
+.pipe(exitOnError(Stringifier()))
+.pipe(exitOnError(process.stdout))
+
