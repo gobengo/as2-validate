a1d20337baa708fc362fc03df382a3828311c7b7
Benjamin Goering <bengoering@gmail.com>
2015-12-02 14:49:34 -0800

    Cleaner

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 var jsonstream = require('JSONStream')
 var validator = require('activitystreams.validator')
-var map = require('stream-transform').map
-
-function exitOnError(stream) {
-  stream.on('error', function (err) {
-    console.error(err);
-    process.exit(1)
-  })
-  return stream;
-}
+var transform = require('stream-transform')
 
 process.stdin
-.pipe(exitOnError(jsonstream.parse()))
-.pipe(exitOnError(map(validator.validate)))
+.pipe(transform.compose(
+  jsonstream.parse(),
+  transform.map(validator.validate)
+))
 .on('end', function () {
   process.exit()
 })
